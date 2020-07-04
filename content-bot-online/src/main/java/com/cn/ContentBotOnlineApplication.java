c96ba926c59dd602bc3324735ac73fd0a18f9a07
18838988676 <18838928050@163.com>
2020-07-05 00:58:06 +0800

    各个模块初始搭建完毕，还未配置

---
+++ content-bot-online/src/main/java/com/cn/ContentBotOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentBotOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentBotOnlineApplication.class, args);
+    }
+
+}
