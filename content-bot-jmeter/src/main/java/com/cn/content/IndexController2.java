3c38abd63168a786dc4199baf880987306ed4fac
18838988676 <18838928050@163.com>
2020-07-13 00:11:07 +0800

    jmeter添加简单测试用例

---
+++ content-bot-jmeter/src/main/java/com/cn/content/IndexController2.java
@@ -0,0 +1,36 @@
+package com.cn.content;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalTime;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 测试jmeter
+ * @Time
+ */
+
+@Controller
+@RequestMapping("/index")
+public class IndexController2 {
+
+    private AtomicInteger num=new AtomicInteger(1);
+
+
+    @RequestMapping("/num")
+    @ResponseBody
+    public  String  index01 (){
+        LocalTime lt = LocalTime.now();
+        String time=lt.getHour()+":"+lt.getMinute()+":"+lt.getSecond();
+        String s=String.format("当前线程:%s,累计访问:%s,时间:%s",Thread.currentThread().getName(),num.getAndIncrement(),time);
+        System.out.println(s);
+        return s;
+    }
+
+
+
+}
